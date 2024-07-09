5e4fca1cd1601432374e7ecd125ebdd68ba5b590
vdt2904 <vudoanhthai12b2002hla@gmail.com>
2024-07-09 11:03:16 +0700

    Add project files.

---
+++ WebComic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebComic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
